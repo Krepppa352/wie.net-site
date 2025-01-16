8132b36fc1995da67adb5ff5868d05118c47120b
Krepppa253 <krepppa253@prometheus-mail.de>
2025-01-16 20:31:14 +0100

    added touchscreen swipe support in gallery (hopefully)

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -7,18 +7,31 @@ async function fetchJson(path) {
     console.log("Fetching finished.");
     return jsonData;
 }
-function swapImage(e) {
-    if (e.code === "ArrowLeft") {
+function swapImage(direction) {
+    if (direction === "left") {
         currentIndex = (currentIndex - 1 + jsonData.length) % jsonData.length;
     }
-    else if (e.code === "ArrowRight") {
+    else if (direction === "right") {
         currentIndex = (currentIndex + 1) % jsonData.length;
     }
     else {
         return;
     }
     document.body.style.backgroundImage = "url(" + jsonData[currentIndex]["path"] + ")";
+    document.getElementById("description").innerHTML = jsonData[currentIndex]["description"]
 }
-
-window.addEventListener("keydown", swapImage);
-fetchJson("../staticdata/gallery.json");
+function swapImageKeyDown(e) {
+    if (e.code === "ArrowLeft") {
+        swapImage("left");
+    }
+    else if (e.code === "ArrowRight") {
+        swapImage("right");
+    }
+    else {return;}
+}
+fetchJson("../staticdata/gallery.json").then(() => {
+    window.addEventListener("keydown", swapImageKeyDown);
+    const hammerObj = new Hammer(document.body);
+    hammerObj.on("swipeleft", swapImage("left"));
+    hammerObj.on("swiperight", swapImage("right"));
+});
